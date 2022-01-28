1d1261f235214088d991cd699293701139b9651e
jk <jakubykriz@gmail.com>
2022-01-29 00:11:33 +0100

    fix correct json response

--- server.go
+++ server.go
@@ -2,28 +2,28 @@ package main
 
 import "net/http"
 import "encoding/json"
+import "log"
 
-type Resource struct {
-	name        string
-	otherattr   string
-	maybeintatr int
+type resource struct {
+		Name        string `json:"name"`
+		Otherattr   string `json:"otherattr"`
+		Maybeintatr int `json:"maybeintatr"`
+}
+
+var testresource *resource = &resource{
+        Name: "test",
+        Otherattr:  "test",
+		Maybeintatr: 8,
 }
 
 func resourceHandler(w http.ResponseWriter, r *http.Request) {
-	var resource Resource
-	resource.name = "test"
-	resource.otherattr = "test2"
-	resource.maybeintatr = 8
-	jsonBytes, err := json.Marshal(resource)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
-	}
+	jsonBytes, _ := json.Marshal(testresource)
 	w.Write(jsonBytes)
 }
 
 func main() {
 	http.HandleFunc("/resource", resourceHandler)
+	log.Println("serving!")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		panic(err)
